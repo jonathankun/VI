c64a6472963306e4d5f0a46b6cb8f7d8fb82b576
jonathankun <jonathanriveraroman@gmail.com>
2019-04-08 15:54:17 -0500

    VI

---
+++ VInspection/VInspection/Resources/NonSwipableViewPager.cs
@@ -0,0 +1,24 @@
+﻿using Android.Content;
+using Android.Support.V4.View;
+using Android.Util;
+using Android.Views;
+
+namespace VInspection.Clases
+{
+    public class NonSwipableViewPager : ViewPager
+    {
+        public NonSwipableViewPager(Context context) : base(context) { }
+
+        public NonSwipableViewPager(Context context, IAttributeSet attrs) : base(context, attrs) { }
+
+        public override bool OnInterceptTouchEvent(MotionEvent ev)
+        {
+            return false;
+        }
+
+        public override bool OnTouchEvent(MotionEvent e)
+        {
+            return false;
+        }
+    }
+}
